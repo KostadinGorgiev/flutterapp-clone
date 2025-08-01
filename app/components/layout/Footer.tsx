58923aa70cce98da6be6b987daaa2b7250179ce6
Kosta <kostadingorgiev91@gmail.com>
2025-08-01 21:06:56 +0800

    feat: implement footer

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,184 +1,101 @@
 import type { FC } from "react"
+import styles from "./Footer.module.scss"
+import Container from "@components/Container"
+import { FaFeatherPointed, FaInstagram } from "react-icons/fa6"
+import { IoMdArrowDropright } from "react-icons/io"
+import { AiOutlineHome } from "react-icons/ai"
+import { RiTwitterXFill } from "react-icons/ri"
+import { BiLogoFacebook } from "react-icons/bi"
 
 const Footer: FC = () => {
     return (
-        <footer className="layout-footer">
-            <div className="container">
-                <div className="footer-inner">
-                    <div className="row">
-                        {/* footer-widget */}
-                        <div className="col-lg-4">
-                            <div className="footer-widget">
-                                <div className="footer-widget-title">Join FlutterPads</div>
-                                <div className="footer-widget-content">
-                                    <p>
-                                        FlutterPads is the first platform{" "}
-                                        <i>Built by Hosts, for Hosts</i>. We know first hand what it
-                                        takes to run rentals, manage guests, and stay profitable. That's
-                                        why FlutterPads flips the script of the industry and gives Hosts
-                                        unparalleled control, earnings potential, and protection
-                                        capacity.
-                                    </p>
-                                    <br />
-                                    <div className="api-links-wrap">
-                                        <a
-                                            href="https://booking.flutterpads.com/mytrips.php"
-                                            className="footer-widget-content-link"
-                                        >
-                                            <span>Become a Host</span>
-                                            <i className="fa-solid fa-feather-pointed" />
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
+        <footer className={styles.footer}>
+            <Container>
+                <div className={styles.footerTop}>
+                    <div className={`${styles.footerItem} ${styles.footerFirst}`}>
+                        <div className={styles.footerHeader}>
+                            Join FlutterPads
                         </div>
-                        {/* footer-widget  end*/}
-                        {/* footer-widget */}
-                        <div className="col-lg-2">
-                            <div className="footer-widget">
-                                <div className="footer-widget-title">Helpful links</div>
-                                <div className="footer-widget-content">
-                                    <div className="footer-list footer-box  ">
-                                        <ul>
-                                            <li>
-                                                <a href="pads.html">All Properties</a>
-                                            </li>
-                                            <li>
-                                                <a href="membership.html">Membership</a>
-                                            </li>
-                                            <li>
-                                                <a href="terms.html">Terms &amp; Conditions</a>
-                                            </li>
-                                            <li>
-                                                <a href="contact.html">Contact Us</a>
-                                            </li>
-                                            <li>
-                                                <a href="https://booking.flutterpads.com/help.php?type=tech-support">
-                                                    Help Center
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
+                        <div className={styles.footerContent}>
+                            <div className={styles.footerContentText}>
+                                FlutterPads is the first platform <i>Built by Hosts</i>, for Hosts. We know first hand what it takes to run rentals, manage guests, and stay profitable. That's why FlutterPads flips the script of the industry and gives Hosts unparalleled control, earnings potential, and protection capacity.
                             </div>
+                            <br />
+                            <button className={styles.footerContentButton}>
+                                <span>Become a Host</span>
+                                <FaFeatherPointed />
+                            </button>
                         </div>
-                        {/* footer-widget  end*/}
-                        {/* footer-widget */}
-                        <div className="col-lg-2">
-                            <div className="footer-widget">
-                                <div className="footer-widget-title">Contact Us</div>
-                                <div className="footer-widget-content">
-                                    <div className="footer-list footer-box  ">
-                                        <ul className="footer-contacts  ">
-                                            <li>
-                                                <span>Email:</span>
-                                                <a href="mailto:info@flutterpads.com" target="_blank">
-                                                    info@flutterpads.com
-                                                </a>
-                                            </li>
-                                            <li>
-                                                {" "}
-                                                <span>Corporate Address:</span>
-                                                <a>
-                                                    <br />
-                                                    1801 Century Park East Los Angeles, CA 90067
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="contact.html">Contact Form</a>
-                                            </li>
-                                        </ul>
-                                        <a href="contact.html" className="footer-widget-content-link">
-                                            <span>Get in Touch</span>
-                                            <i className="fa-solid fa-caret-right" />
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {/* footer-widget  end*/}
-                        {/* footer-widget */}
-                        <div className="col-lg-4">
-                            <div className="footer-widget">
-                                <div className="footer-widget-title">Subscribe</div>
-                                <div className="footer-widget-content">
-                                    <p>
-                                        Want to be notified when we launch a new property or have an
-                                        update to share? Subscribe below and we'll send you a
-                                        notification by email.
-                                    </p>
-                                    <div
-                                        style={{
-                                            width: "100%",
-                                            height: 273,
-                                            position: "relative",
-                                            transition: "height 150ms"
-                                        }}
-                                        data-fillout-id="3Agoej25Wgus"
-                                        data-fillout-embed-type="standard"
-                                        data-fillout-inherit-parameters=""
-                                        data-fillout-dynamic-resize=""
-                                        data-fillout-initialized="true"
-                                    >
-                                        <div className="fillout-embed-standard" style={{ opacity: 1 }}>
-                                            <div
-                                                className="fillout-embed-loading"
-                                                style={{ display: "none" }}
-                                            />
-                                            <div
-                                                className="fillout-embed-iframe-container"
-                                                style={{ opacity: 1 }}
-                                            >
-                                                <iframe
-                                                    src="https://embed.fillout.com/t/3Agoej25Wgus?fillout-embed-id=64976129007991&fillout-embed-type=standard&fillout-embed-parent-page=https%3A%2F%2Fwww.flutterpads.com%2F&fillout-embed-dynamic-resize=true"
-                                                    allow="microphone; camera; geolocation"
-                                                    title="3Agoej25Wgus"
-                                                    style={{ border: 0 }}
-                                                />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {/* footer-widget  end*/}
                     </div>
-                    {/* footer-widget-wrap end*/}
-                </div>
-                <div className="footer-bottom">
-                    <a href="index.html" className="footer-home_link">
-                        <i className="fa-regular  fa-house" />
-                    </a>
-                    <div className="copyright">
-                        {" "}
-                        <span>
-                            © FlutterPads <span id="currentYear">2025</span>
-                        </span>
-                        . All rights reserved.{" "}
-                    </div>
-                    <div className="footer-social">
-                        <span className="footer-social-title">Follow Us</span>
-                        <div className="footer-social-wrap">
-                            <a
-                                href="https://www.facebook.com/p/FlutterPads-61552661315592/"
-                                target="_blank"
-                            >
-                                <i className="fa-brands fa-facebook-f" />
-                            </a>
-                            <a href="https://twitter.com/FlutterPads" target="_blank">
-                                <i className="fa-brands fa-x-twitter" />
-                            </a>
-                            <a href="https://instagram.com/Flutter.Pads" target="_blank">
-                                <i className="fa-brands fa-instagram" />
-                            </a>
-                            {/* <a href="#" target="_blank"><i class="fa-brands fa-tiktok"></i></a> */}
-                            {/* <a href="#" target="_blank"><i class="fa-brands fa-youtube"></i></a>										  */}
+                    <div className={`${styles.footerItem} ${styles.footerSecond}`}>
+                        <div className={styles.footerHeader}>
+                            Helpful links
                         </div>
+                        <div className={styles.footerContent}>
+                            <div className={styles.footerLinkList}>
+                                <div className={styles.footerLinkItem}><div className={styles.footerLinkIcon}><IoMdArrowDropright /></div><a href="#">All Properties</a></div>
+                                <div className={styles.footerLinkItem}><div className={styles.footerLinkIcon}><IoMdArrowDropright /></div><a href="#">Membership</a></div>
+                                <div className={styles.footerLinkItem}><div className={styles.footerLinkIcon}><IoMdArrowDropright /></div><a href="#">Terms & Conditions</a></div>
+                                <div className={styles.footerLinkItem}><div className={styles.footerLinkIcon}><IoMdArrowDropright /></div><a href="#">Contact Us</a></div>
+                                <div className={styles.footerLinkItem}><div className={styles.footerLinkIcon}><IoMdArrowDropright /></div><a href="#">Help Center</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={`${styles.footerItem} ${styles.footerThird}`}>
+                        <div className={styles.footerHeader}>
+                            Contact Us
+                        </div>
+                        <div className={styles.footerContent}>
+                            <div className={styles.footerLinkList}>
+                                <div className={styles.footerLinkItem}><div className={styles.footerLinkIcon}><IoMdArrowDropright /></div><a href="#"><span>Email:</span>info@flutterpads.com</a></div>
+                                <div className={styles.footerLinkItem}><div className={styles.footerLinkIcon}><IoMdArrowDropright /></div><a href="#"><span>Corporate Address:</span><br /><br />1801 Century Park East Los Angeles, CA 90067</a></div>
+                                <div className={styles.footerLinkItem}><div className={styles.footerLinkIcon}><IoMdArrowDropright /></div><a href="#">Contact Form</a></div>
+                            </div>
+                            <button className={`${styles.footerContentButton} ${styles.footerContentButtonContact}`}>
+                                <span>Get in Touch</span>
+                                <IoMdArrowDropright className={styles.footerContentButtonIcon} />
+                            </button>
+                        </div>
+                    </div>
+                    <div className={`${styles.footerItem} ${styles.footerFourth}`}>
+                        <div className={styles.footerHeader}>
+                            Subscribe
+                        </div>
+                        <div className={styles.footerContent}>
+                            <div className={styles.footerContentText}>
+                                Want to be notified when we launch a new property or have an update to share? Subscribe below and we'll send you a notification by email.
+                            </div>
+                        </div>
+                        <iframe src="https://embed.fillout.com/t/3Agoej25Wgus?fillout-embed-id=64976129007991&amp;fillout-embed-type=standard&amp;fillout-embed-parent-page=https%3A%2F%2Fwww.flutterpads.com%2F&amp;fillout-embed-dynamic-resize=true" allow="microphone; camera; geolocation" title="3Agoej25Wgus" style={{ border: "0px" }}></iframe>
                     </div>
                 </div>
-            </div>
+                <div className={styles.footerBottom}>
+                    <div className={styles.footerBottomLeft}>
+                        <a href="/" className={styles.footerBottomHomeLink}><AiOutlineHome /></a>
+                        <p className={styles.footerBottomText}>
+                            &copy; FlutterPads 2025. All rights reserved.
+                        </p>
+                    </div>
+                    <div className={styles.footerBottomRight}>
+                        <p className={styles.footerBottomText}>
+                            Follow Us
+                        </p>
+                        <div className={styles.footerBottomSocial}>
+                            <a href="#">
+                                <BiLogoFacebook />
+                            </a>
+                            <a href="#">
+                                <RiTwitterXFill />
+                            </a>
+                            <a href="#">
+                                <FaInstagram />
+                            </a>
+                        </div>
+                    </div>
+                    <div className={styles.footerBottomSeparator}></div>
+                </div>
+            </Container>
         </footer>
-
     )
 }
 
