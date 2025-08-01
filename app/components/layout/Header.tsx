6a1b2261423cd27ed137b9440deb76dd66f0a0da
Kosta <kostadingorgiev91@gmail.com>
2025-08-01 17:47:31 +0800

    finalize header

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -5,7 +5,8 @@ import Logo from '@assets/images/wordmark-tagline-black.png'
 
 import styles from './Header.module.scss';
 
-import { FaBolt } from "react-icons/fa";
+import { FaBolt } from "react-icons/fa6";
+import { IoMdClose } from "react-icons/io";
 
 const Header: FC = () => {
 
@@ -29,9 +30,35 @@ const Header: FC = () => {
                         <a href="contact.html">Contact</a>
                         <a href="invest.html">Invest</a>
                         <a href="https://booking.flutterpads.com/managers.php">Host Dashboard</a>
-                        <a href="https://booking.flutterpads.com/signup.php" className={styles.listBtn}>
-                            <span>List Your Property</span>
-                        </a>
+                    </div>
+                    <a href="https://booking.flutterpads.com/signup.php" className={styles.listBtn}>
+                        <span>List Your Property</span>
+                    </a>
+                    <div className={styles.mobileMenu}>
+                        <div className={`${styles.menuBtn} ${showMenu ? styles.active : ''}`} onClick={() => setShowMenu(!showMenu)}>
+                            <div className={styles.menuBtnIcon}>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                            <div className={styles.closeIcon}>
+                                <IoMdClose />
+                            </div>
+                        </div>
+                        <div className={`${styles.menuList} ${showMenu ? styles.active : ''}`}>
+                            <a href="index.html" className={styles.activeLink}>Home</a>
+                            <a href="pads.html">Pads</a>
+                            <a href="https://travel.flutterpads.com" target="_blank" rel="noopener noreferrer">Hotels</a>
+                            <a href="https://travel.flutterpads.com" target="_blank" rel="noopener noreferrer">Rental Cars</a>
+                            <a href="https://travel.flutterpads.com" target="_blank" rel="noopener noreferrer">Flights</a>
+                            <a href="membership.html">Become a Host<FaBolt /></a>
+                            <a href="contact.html">Contact</a>
+                            <a href="invest.html">Invest</a>
+                            <a href="https://booking.flutterpads.com/managers.php">Host Dashboard</a>
+                            <a href="https://booking.flutterpads.com/signup.php" className={styles.listBtn}>
+                                <span>List Your Property</span>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </Container>
